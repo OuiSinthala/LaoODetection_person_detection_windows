7c7ca773578849afafb6bba120799e8c0f3b1bf0
Sinthala <sinthala.og.tsg@gmail.com>
2023-05-24 19:11:08 +0700

    Update sound_config.py

--- LaoODetection/sound_config.py
+++ LaoODetection/sound_config.py
@@ -1,12 +1,10 @@
 import winsound
 
-def play_sound(file_path):
-    winsound.PlaySound(file_path, winsound.SND_FILENAME)
+def play_sound():
+    # Provide the path to the sound file
+    sound_file_path = "C:\\Users\\PC\\OneDrive\\Desktop\\LaoODetection_person_detection\\LaoODetection\\sounds\\hello1.wav"
+    winsound.PlaySound(sound_file_path, winsound.SND_FILENAME)
 
-# Provide the path to the sound file
-sound_file_path = "C:\\Users\\PC\\OneDrive\\Desktop\\LaoODetection_person_detection\\LaoODetection\\sounds\\hello1.wav"
 
-# Play the sound
-play_sound(sound_file_path)
 
 
